09610a2812850f440f6743a8da9c2187de6ead1d
Osamaemam79 <159637695+Osamaemam79@users.noreply.github.com>
2024-02-10 20:17:28 +0200

    Update johndoe.js

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -57,7 +57,7 @@ $(window).on("load", function() {
 
 
 // google maps
-/*/function initMap() {
+    function initMap() {
 // Styles a map in night mode.
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.674, lng: -73.945},
@@ -148,4 +148,3 @@ $(window).on("load", function() {
       ]
     });
 }
-*
